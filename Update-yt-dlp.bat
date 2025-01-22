fe1f9ce0be031bc9b5e9f918dd3b6147c2e34552
Alexey <10572914+Alexey71@users.noreply.github.com>
2025-01-22 12:52:51 +0300

    Add files via upload

---
+++ Update-yt-dlp.bat
@@ -0,0 +1,10 @@
+@echo off
+title Обновление yt-dlp
+echo.
+echo ***************************************
+echo       Проверка и обновление yt-dlp
+echo ***************************************
+echo.
+"yt-dlp" --update-to master
+echo.
+exit
